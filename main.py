83801531fe3060bd24b5a92541fe4f501a997cb5
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-21 13:31:16 +0530

    Added Generators for Random MAC Address and String

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
+import string
 from os import geteuid
 from scapy.all import *
 from datetime import date
+from random import choice
 from optparse import OptionParser
 from colorama import Fore, Back, Style
 from time import strftime, localtime
@@ -15,6 +17,9 @@ status_color = {
     ' ': Fore.WHITE
 }
 
+allowed_characters = string.ascii_letters + "1234567890"
+mac_elements = "0123456789ABCDEF"
+
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
@@ -26,6 +31,11 @@ def get_arguments(*args):
 
 def check_root():
     return geteuid() == 0
+
+def generateRandomMAC():
+    return ':'.join([f"{choice(mac_elements)}{choice(mac_elements)}" for _ in range(6)])
+def generateRandomString(length):
+    return ''.join([choice(allowed_characters) for _ in range(length)])
 
 if __name__ == "__main__":
     pass
