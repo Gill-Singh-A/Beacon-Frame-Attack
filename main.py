380fab568ea0f706536fe6462dbbdf61283b5e2c
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-21 15:41:24 +0530

    Implemented Command Line Arguments

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ status_color = {
 allowed_characters = string.ascii_letters + "1234567890"
 mac_elements = "0123456789abcdef"
 broadcast_mac = "ff:ff:ff:ff:ff:ff"
+send_interval_delay = 0.5
 
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
@@ -46,4 +47,39 @@ def sendBeaconFrame(ssid, mac, iface, count, interval):
     sendp(packet, iface=iface, count=count, inter=interval, verbose=False)
 
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-i', "--interface", "interface", "Network Interface to Start Sniffing on"),
+                              ('-e', "--essid", "essid", "ESSID for the Beacon Frame (Seperated by '~' and mac seperated by ',' (essid_0,mac_0~essid_1,mac_1) or File containing List of ESSIDs and MAC Addresses (essid,mac) (MAC Address is Optional))"),
+                              ('-m', "--mac", "mac", "MAC Addresses for ESSIDs (Seperated by ',' or File Containing List of MAC Addresses, Default=Random)"),
+                              ('-d', "--delay", "delay", f"Delay Between Channel Hopping (Default={send_interval_delay} seconds)"),
+                              ('-w', "--write", "write", "Dump Packets to a File"))
+    if not check_root():
+        display('-', f"This Program requires {Back.YELLOW}root{Back.RESET} Privileges")
+        exit(0)
+    if not arguments.interface or arguments.interface not in get_if_list():
+        display('-', "Please specify a Valid Interface")
+        display('*', f"Available Interfaces : {Back.MAGENTA}{','.join(get_if_list())}{Back.RESET}")
+        exit(0)
+    if not arguments.delay:
+        arguments.delay = send_interval_delay
+    else:
+        arguments.delay = float(arguments.delay)
+    if arguments.essid:
+        try:
+            with open(arguments.essid, 'r') as file:
+                arguments.essid = {line.split(',')[0]: '' if len(line.split(',')) == 1 else line.split(',')[1] for line in file.read().split('\n')}
+        except FileNotFoundError:
+            arguments.essid = {essid_mac.split(',')[0]: '' if len(essid_mac.split(',')) == 1 else essid_mac.split(',')[1] for essid_mac in arguments.essid.split('~')}
+        except:
+            display('-', f"Error while Reading File {Back.YELLOW}{arguments.essid}{Back.RESET}")
+            exit(0)
+    if arguments.mac:
+        arguments.mac = arguments.mac.split(',')
+        total_macs = len(arguments.mac)
+        current_mac_index = 0
+        for essid, mac in arguments.essid.items():
+            if mac == '':
+                arguments.essid[essid] = arguments.mac[current_mac_index%total_macs]
+                current_mac_index += 1
+    for essid, mac in arguments.essid.items():
+        if mac == '':
+            arguments.essid[essid] = generateRandomMAC()
