549c67fa84603baf23f6c93bcbd2e1f5980b75b1
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-21 15:26:31 +0530

    Implemented Function to send Beacon Frame

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ status_color = {
 }
 
 allowed_characters = string.ascii_letters + "1234567890"
-mac_elements = "0123456789ABCDEF"
+mac_elements = "0123456789abcdef"
+broadcast_mac = "ff:ff:ff:ff:ff:ff"
 
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
@@ -36,6 +37,13 @@ def generateRandomMAC():
     return ':'.join([f"{choice(mac_elements)}{choice(mac_elements)}" for _ in range(6)])
 def generateRandomString(length):
     return ''.join([choice(allowed_characters) for _ in range(length)])
+
+def sendBeaconFrame(ssid, mac, iface, count, interval):
+    dot11 = Dot11(type=0, subtype=8, addr1=broadcast_mac, addr2=mac, addr3=mac)
+    beacon = Dot11Beacon()
+    essid = Dot11Elt(ID="SSID", info=ssid, len=len(ssid))
+    packet = RadioTap() / dot11 / beacon / essid
+    sendp(packet, iface=iface, count=count, inter=interval, verbose=False)
 
 if __name__ == "__main__":
     pass
